74f23991799d45ee669257ebd4e79696e0f8d054
jair <jairenriquezalva@gmail.com>
2019-11-22 18:33:49 -0500

    se modifico las politicas cors

--- Startup.cs
+++ Startup.cs
@@ -28,6 +28,12 @@ namespace ReserB
 		// This method gets called by the runtime. Use this method to add services to the container.
 		public void ConfigureServices(IServiceCollection services)
 		{
+			services.AddCors(o => o.AddPolicy("MyPolicy", builder =>
+			{
+				builder.AllowAnyOrigin()
+					   .AllowAnyMethod()
+					   .AllowAnyHeader();
+			}));
 			services.AddDistributedMemoryCache();
 			services.AddSession();
 			services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
@@ -55,6 +61,7 @@ namespace ReserB
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
 		public void Configure(IApplicationBuilder app, IHostingEnvironment env)
 		{
+			app.UseCors("MyPolicy");
 			if (env.IsDevelopment())
 			{
 				app.UseDeveloperExceptionPage();
